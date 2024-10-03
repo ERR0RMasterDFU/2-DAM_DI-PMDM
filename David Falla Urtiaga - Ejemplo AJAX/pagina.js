05068d078513d6fbd06f3097aa5745475ea84dfb
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-03 10:01:36 +0200

    Ejemplo de Ajax y ejercicio 2 creados

---
+++ David Falla Urtiaga - Ejemplo AJAX/pagina.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+
+    $.ajax({
+
+      url: "https://pokeapi.co/api/v2/pokemon",
+      method: "GET",
+
+    }).done(function (data) {
+
+      /* JSON.parse() toma como entrada el texto en JSON
+        que me llega del servidor y lo convierte a un objeto
+      */
+      let json = JSON.parse(data);
+
+      debugger;
+      
+      /* Lo que se programa dentro de esta función
+         será ejecutado cuando se haya resuelto
+         la petición asíncrona, es decir, cuando
+         llegue la respuesta del servidor.
+         */
+    });
+  });
