f8408ec76714f81f9cef26fdf9aa93988d27fc22
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-15 20:39:37 +0200

    Calculadora completada

--- EJERCICIOS/ANGULAR/calculadora/src/app/components/calculadora/calculadora.component.ts
+++ EJERCICIOS/ANGULAR/calculadora/src/app/components/calculadora/calculadora.component.ts
@@ -20,11 +20,67 @@ export class CalculadoraComponent {
   ocho = 8;
   nueve = 9;
 
+  numero1 = 0;
+  numero2 = 0;
+  resultado = 0;
+
+  postOperacion = 0;
+  suma = false;
+  resta = false;
+
+  mostrarEnPantalla() {
+  
+    switch (this.postOperacion) {
+      case 1:
+        return this.resultado;
+        break;
+
+      case 2:
+        return this.numero2;
+        break;
+
+      default:
+        return this.numero1;
+        break;
+    }
+  }
+
+  borrarTodo () {
+    this.numero1 = 0;
+    this.numero2 = 0;
+    this.resultado = 0;
+    this.postOperacion = 0;
+    this.suma = false;
+    this.resta = false;
+  }
+
+  seleccionarNumero1(numero:number){
+    this.numero1 = numero;
+  }
+
+  seleccionarNumero2(numero:number){
+    this.numero2 = numero;
+  }
+
   sumar() {
-    
+    this.postOperacion = 2;
+    this.suma = true;
+  }
+
+  restar() {
+    this.postOperacion = 2;
+    this.resta = true;
   }
 
 
+  operacion() {
+    this.postOperacion = 1;
+    if(this.suma==true){
+      this.resultado = this.numero1 + this.numero2;
+    }else{
+      this.resultado = this.numero1 - this.numero2;
+    }
+  }
 
 
 }
