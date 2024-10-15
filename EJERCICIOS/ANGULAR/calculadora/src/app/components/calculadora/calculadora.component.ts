97b054316b7040d02300b071c58c2de8341cfd0d
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-15 10:59:56 +0200

    Ejercicio calcu

---
+++ EJERCICIOS/ANGULAR/calculadora/src/app/components/calculadora/calculadora.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculadora',
+  standalone: true,
+  imports: [],
+  templateUrl: './calculadora.component.html',
+  styleUrl: './calculadora.component.css'
+})
+export class CalculadoraComponent {
+
+  cero = 0;
+  uno = 1;
+  dos = 2;
+  tres = 3;
+  cuatro = 4;
+  cinco = 5;
+  seis = 6;
+  siete = 7;
+  ocho = 8;
+  nueve = 9;
+
+  sumar() {
+    
+  }
+
+
+
+
+}
