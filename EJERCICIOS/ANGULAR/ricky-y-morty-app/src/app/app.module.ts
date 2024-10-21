7d9c7b91bf4934951690b766f1d7cc4fbb8df376
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2024-10-21 12:27:31 +0200

    App de Ricky y Morty creada

---
+++ EJERCICIOS/ANGULAR/ricky-y-morty-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CharactersListComponentComponent } from './components/characters-list-component/characters-list-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CharactersListComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ EJERCICIOS/ANGULAR/ricky-y-morty-app/src/app/components/characters-list-component/characters-list-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-list-component',
+  templateUrl: './characters-list-component.component.html',
+  styleUrl: './characters-list-component.component.css'
+})
+export class CharactersListComponentComponent {
+
+}
